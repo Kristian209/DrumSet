e62d761a73409f03d740c92992995143a513bbd5
Kristian Estrada <KristianEstrada209@gmail.com>
2018-09-09 13:24:20 -0700

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
